4e0552ec1dff7dbe9aaab94bce5a1fd9ae1e6607
Fredy <megablastoise94@gmail.com>
2018-11-15 17:44:30 -0500

    Updates

---
+++ precache-manifest.d8c31c99d4b73cba1f944bfcfec237c4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a07216b3b96b3a9e1959c7d269212bd6",
+    "url": "/my-portfolio/static/media/icomoon.a07216b3.ttf"
+  },
+  {
+    "revision": "cd60386a4f95825ed28e",
+    "url": "/my-portfolio/static/css/main.97f3defb.chunk.css"
+  },
+  {
+    "revision": "e9124ddf86bb13a82835",
+    "url": "/my-portfolio/static/js/1.e9124ddf.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "05b2505acad528bc9d0361ff710cac5e",
+    "url": "/my-portfolio/static/media/logoSebas.05b2505a.PNG"
+  },
+  {
+    "revision": "8e51104a14b75f98935507ae38dc7725",
+    "url": "/my-portfolio/static/media/Sebastian.8e51104a.gif"
+  },
+  {
+    "revision": "f055601c1a7528df946e12ab58d1b07a",
+    "url": "/my-portfolio/static/media/Ashara Games Blanco.f055601c.png"
+  },
+  {
+    "revision": "1b7f5835c1a2b9dddc0d9054e920d459",
+    "url": "/my-portfolio/static/media/icomoon.1b7f5835.eot"
+  },
+  {
+    "revision": "6e394b393f43332589311de6c73d42dc",
+    "url": "/my-portfolio/static/media/icomoon.6e394b39.svg"
+  },
+  {
+    "revision": "cd60386a4f95825ed28e",
+    "url": "/my-portfolio/static/js/main.cd60386a.chunk.js"
+  },
+  {
+    "revision": "fb3915d6bbf5b09fb46bea711a39b421",
+    "url": "/my-portfolio/static/media/icomoon.fb3915d6.woff"
+  },
+  {
+    "revision": "ac6e9cdd48e2432bc08e092c7809a8ef",
+    "url": "/my-portfolio/static/media/twitter.ac6e9cdd.svg"
+  },
+  {
+    "revision": "ea1b815ecbbd285726fd6cd2f487edc4",
+    "url": "/my-portfolio/static/media/facebook.ea1b815e.svg"
+  },
+  {
+    "revision": "60d166b38db33f894bf7555a199ca010",
+    "url": "/my-portfolio/static/media/instagram.60d166b3.svg"
+  },
+  {
+    "revision": "750dac63675afe3a04bd74fa0a8a1bee",
+    "url": "/my-portfolio/static/media/github.750dac63.svg"
+  },
+  {
+    "revision": "e966da8dab2b41d26302fbbc80f03e1c",
+    "url": "/my-portfolio/static/media/linkedin-logo.e966da8d.svg"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "/my-portfolio/static/media/envelope.e9419679.svg"
+  },
+  {
+    "revision": "5080129d4d5f49d6bf2dad7f3ee1927a",
+    "url": "/my-portfolio/index.html"
+  }
+];
