6159b74e3268d2275c5feecd48f20ef785733407
Fredy <megablastoise94@gmail.com>
2020-05-21 10:39:47 -0500

    Se actualiza visualmente el sitio, se actualizan las redes sociales y se agrega un nuevo proyecto al portafolio de games

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -7,10 +7,10 @@ class Contacto extends Component {
       <div className="Contacto">
       <div className="container">
         <div className="social">
-          <a href="https://twitter.com/Kimbley5" rel="noopener noreferrer" className="social-link twitter" target="_blank"> </a>
-          <a href="https://www.facebook.com/gamefred" rel="noopener noreferrer" className="social-link facebook" target="_blank"> </a>
-          <a href="https://github.com/SolfKimbley"  rel="noopener noreferrer" className="social-link github" target="_blank"> </a>
-          <a href="https://www.instagram.com/zolfkimbley" rel="noopener noreferrer" className="social-link instagram" target="_blank"> </a>
+          <a href="https://twitter.com/chimpabastian" rel="noopener noreferrer" className="social-link twitter" target="_blank"> </a>
+          <a href="https://www.facebook.com/chimpabastian" rel="noopener noreferrer" className="social-link facebook" target="_blank"> </a>
+          <a href="https://github.com/FredyRodriguezR"  rel="noopener noreferrer" className="social-link github" target="_blank"> </a>
+          <a href="https://www.instagram.com/chimpabastian" rel="noopener noreferrer" className="social-link instagram" target="_blank"> </a>
           <a href="https://www.linkedin.com/in/fredy-sebastian-rodriguez-rincon-b11239129/" rel="noopener noreferrer" className="social-link linkedin" target="_blank"> </a>
           <a href="https://steamdb.info/calculator/76561198042289628/?cc=co" rel="noopener noreferrer" className="social-link steam" target="_blank"> </a>
         </div>
